10d2e254f42a84b33f4d0d0144b62cf2905adeea
iosDG001 <1870299978@qq.com>
2024-09-13 16:17:02 +0800

    更新 VPN.js

---
+++ VPN.js
@@ -0,0 +1,50 @@
+[rewrite_local] 
+
+^https?:\/\/api\.revenuecat\.com\/v1\/receipts url script-response-body https://raw.githubusercontent.com/iosDG001/_/main/VPN.js
+
+[MITM]
+hostname = api.revenuecat.com
+
+
+*/
+{
+  "request_date" : "2024-09-13T08:07:50Z",
+  "request_date_ms" : 1726214870737,
+  "subscriber" : {
+    "non_subscriptions" : {
+
+    },
+    "first_seen" : "2024-09-13T06:40:55Z",
+    "original_application_version" : "3",
+    "other_purchases" : {
+
+    },
+    "management_url" : null,
+    "subscriptions" : {
+      "com.ddgksf2013.premium.yearly" : {
+        "is_sandbox" : false,
+        "ownership_type" : "PURCHASED",
+        "billing_issues_detected_at" : null,
+        "period_type" : "normal",
+        "expires_date" : "2099-12-18T01:04:17Z",
+        "grace_period_expires_date" : null,
+        "unsubscribe_detected_at" : null,
+        "original_purchase_date" : "2022-09-08T01:04:18Z",
+        "purchase_date" : "2022-09-08T01:04:17Z",
+        "store" : "app_store"
+      }
+    },
+    "entitlements" : {
+      "pro" : {
+        "expires_date" : "2099-12-18T01:04:17Z",
+        "purchase_date" : "2022-09-08T01:04:17Z",
+        "product_identifier" : "com.ddgksf2013.premium.yearly",
+        "grace_period_expires_date" : null
+      }
+    },
+    "original_purchase_date" : "2024-09-13T06:39:06Z",
+    "original_app_user_id" : "$RCAnonymousID:7f4d7dc22f6f45668b6ec58ccf21c025",
+    "last_seen" : "2024-09-13T08:04:31Z"
+  },
+  "Attention" : "禁止分享"
+}
