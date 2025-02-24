721716dd76eb6697e56bfcd4964bede3ec134abf
iosDG001 <1870299978@qq.com>
2025-02-24 17:12:57 +0800

    更新 Easy.js

--- Easy.js
+++ Easy.js
@@ -1,13 +1,7 @@
-/*        
-       [Easy系列通用VIP]
-
+/*               [Easy系列通用VIP]
 [rewrite_local] 
 https:\/\/(?:easyjapanese\.net|api\.todaienglish\.com|api-german\.todainews\.com|api-spanish\.todainews\.com|api\.todaikorean\.com)\/api\/(?:auth|user)\/(?:init-login|login) url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/Easy.js
-
 [MITM]
 hostname = api.todaikorean.com, api-spanish.todainews.com, api-german.todainews.com, api.aimusics.immusician.com, api2.mubu.com, api.todaienglish.com,easyjapanese.net
-
-
  */
- 
 ;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 0=$7.0;0=0.2(/"4"\\s*:\\s*(?:8|"[^"]*")/g,\'"4": "9://a.b.c/d/e/f/h/i.j"\');0=0.2(/("k"\\s*:\\s*")[^"]*"/g,\'$1爱玩🌹"\');0=0.2(/("l"\\s*:\\s*")[^"]*"/g,\'$1爱玩🌹"\');0=0.2(/("m"\\s*:\\s*")[^"]*"/g,\'$n-3-3 3:3:3"\');0=0.2(/("5"\\s*:\\s*")[^"]*"/g,\'$o"\');0=0.2(/("5"\\s*:\\s*)p/g,\'$q\');$r({0});',29,29,'body||replace|09|image|is_premium|let|response|null|http|oss|v2dj|com|pic|2025|01||12|17366687878571|jpeg|name|username|premium_expired|12999|11|false|1true|done|'.split('|'),0,{}));
