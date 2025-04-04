8effd5284e6e649a806bc2362cfa445d1d173083
iosDG001 <1870299978@qq.com>
2025-04-04 14:57:46 +0800

    更新 XSKD.js

---
+++ XSKD.js
@@ -0,0 +1,7 @@
+ /*                 【销售开单】
+[rewrite_local] 
+^https?:\/\/wxapi\.salekd\.com\/m\.api url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/XSKD.js
+[MITM]
+hostname = wxapi.salekd.com
+*/
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('4 0=$5.0;0=0.2(/("6"\\1*:\\1*)\\7+/3,\'$8\');0=0.2(/("9"\\1*:\\1*)a/3,\'$b\');$c({0});',13,13,'body|s|replace|g|let|response|level|d|12|gmtVipExpire|null|14092610661000|done'.split('|'),0,{}));
