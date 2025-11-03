e480ed711dbc1c02c75852d43b46f8b22d8e9572
iosDG001 <1870299978@qq.com>
2025-11-03 16:22:56 +0800

    更新 ddqd.js

---
+++ ddqd.js
@@ -0,0 +1,16 @@
+/*        
+        ➪：滴答清单
+
+        ★：解锁永久🆅🅸🅿
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+(^https?:\/\/dida365\.com\/api\/v2\/user\/(?:status|profile) url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/ddqd.js
+
+[MITM]
+hostname = dida365.com
+
+
+*/
+;eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('f 0=$h.0;7 4={\'i\':\'j://k.l.m/n/o/p/q/r.t\',\'u\':\'𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉\',\'v\':\'w-x-y:8:8.z+A\'};7 5={\'B\':\'6\',\'C\':\'3\',\'D\':\'3\',\'E\':\'3\',\'F\':\'6\',\'G\':\'3\'};9.a(4).b(2=>{0=0.c(d e(`("${2}"\\\\s*:\\\\s*)"[^"]*"`, \'g\'), `$1"${4[2]}"`);\n});\n9.a(5).b(2 => {\n  0 = 0.c(d e(`("${2}"\\\\s*:\\\\s*)(3|6)`,\'g\'),`$1${5[2]}`)});$H({0});',44,44,'body||key|true|stringFields|boolFields|false|const|59|Object|keys|forEach|replace|new|RegExp|let||response|picture|http|oss|v2dj|com|pic|2025|07|18|17528094565169||jpeg|name|proEndDate|2099|12|31T23|000|0000|needSubscribe|pro|teamPro|teamUser|freeTrial|activeTeamUser|done'.split('|'),0,{}));
