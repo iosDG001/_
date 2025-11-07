6c5f493fac91812f94c257241521a6730da62798
iosDG001 <1870299978@qq.com>
2025-11-07 11:23:05 +0800

    更新 WZPP.js

---
+++ WZPP.js
@@ -0,0 +1,13 @@
+/*        
+        ➪：问真八字排盘
+
+        ★：解锁永久🆅🅸🅿
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/bzpp3\.iwzbz\.com\/api\/v1\.1\/user\/getvipinfo_nv1? url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/WZPP.js
+[MITM]
+hostname = bzpp3.iwzbz.com
+*/
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 0=$6.0;0=0.3(/("7"\\2*:\\2*)\\8+/4,\'$9\');0=0.3(/("a"\\2*:\\2*")[^"]*"/4,\'$b-1-1 1:1:1"\');$c({0});',13,13,'body|09|s|replace|g|let|response|vipLevel|d|13|expires|12999|done'.split('|'),0,{}));
