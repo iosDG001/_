a67ce07bae59f5a6e77d94376cdca9460c0caece
iosDG001 <1870299978@qq.com>
2025-11-21 22:32:37 +0800

    更新 Flightradar24.js

---
+++ Flightradar24.js
@@ -0,0 +1,15 @@
+
+
+/*        
+        ➪：Flightradar24
+
+        ★：去全局广告
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/mobile\.flightradar24\.com\/mobile\/user-session? url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/Flightradar24.js
+[MITM]
+hostname = mobile mobile.flightradar24.com.com
+*/
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('4 0=$5.0;0=0.2(/("6"\\1*:\\1*")[^"]*"/3,\'$7"\');0=0.2(/("8"\\1*:\\1*")[^"]*"/3,\'$9"\');$a({0});',11,11,'body|s|replace|g|let|response|accountType|1GoId|adverts|1disabled|done'.split('|'),0,{}));
