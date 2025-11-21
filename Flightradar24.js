17678e0eecb5a7a7708402305d169ee1c7167113
iosDG001 <1870299978@qq.com>
2025-11-21 22:43:46 +0800

    更新 Flightradar24.js

--- Flightradar24.js
+++ Flightradar24.js
@@ -10,6 +10,6 @@
 [rewrite_local] 
 ^https?:\/\/mobile\.flightradar24\.com\/mobile\/user-session? url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/Flightradar24.js
 [MITM]
-hostname = mobile mobile.flightradar24.com.com
+hostname = mobile.flightradar24.com
 */
 ;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('4 0=$5.0;0=0.2(/("6"\\1*:\\1*")[^"]*"/3,\'$7"\');0=0.2(/("8"\\1*:\\1*")[^"]*"/3,\'$9"\');$a({0});',11,11,'body|s|replace|g|let|response|accountType|1GoId|adverts|1disabled|done'.split('|'),0,{}));
