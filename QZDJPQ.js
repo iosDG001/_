dbce097f90dc6bf7439da4ac6d9abe2b697f3a9a
iosDG001 <1870299978@qq.com>
2025-12-11 14:01:53 +0800

    更新 QZDJPQ.js

---
+++ QZDJPQ.js
@@ -0,0 +1,18 @@
+/*        
+        ➪：全自动记牌器
+
+        ★：解锁永久🆅🅸🅿
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/api\.qzdjpq\.com\/api\/v1\/card_android\/sessions? url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/QZDJPQ.js
+
+[MITM]
+hostname = api.qzdjpq.com
+
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('4 0=$5.0;0=0.2(/("6"\\1*:\\1*)\\d+/g,\'$7\');0=0.2(/("8"\\1*:\\1*)\\d+/g,\'$9\');0=0.2(/("a"\\1*:\\1*)\\d+/g,\'$b\');0=0.2(/("c"\\1*:\\1*)\\d+/g,\'$e\');0=0.2(/("f"\\1*:\\1*)\\d+/g,\'$h\');0=0.2(/("i"\\1*:\\1*)\\d+/g,\'$j\');0=0.2(/("k"\\1*:\\1*)\\d+/g,\'$3\');0=0.2(/("l"\\1*:\\1*)\\d+/g,\'$3\');0=0.2(/("m"\\1*:\\1*)\\d+/g,\'$3\');$n({0});',24,24,'body|s|replace|11|let|response|freeTryDrawState|1888888888|normalVip|188888|ticketsNum|199999|ultimateVip||17777777|advanceVip||166666666|purchaseTimes|199999999999|normalNum|advanceNum|ultimateNum|done'.split('|'),0,{}));
