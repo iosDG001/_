7660a1e43557b2a2db88fa89709e1b5a9604c12a
iosDG001 <1870299978@qq.com>
2025-12-22 22:53:24 +0800

    更新 YJB.js

---
+++ YJB.js
@@ -0,0 +1,9 @@
+^https?:\/\/.*yangjibao\.com\/(wxapi\/)?account url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/YJB.js
+
+
+[mitm] 
+
+hostname= *.yangjibao.com
+
+***********************************/
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('8 0=$9.0.1(/3":4/2,\'3":5\').1(/6":a/2,\'6":"b-c-d"\').1(/7":4/2,\'7":5\');$e({0});',15,15,'body|replace|g|vip_label|false|true|vip_expiry_date|is_pay|var|response|null|2099|12|31|done'.split('|'),0,{}));
