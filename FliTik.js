2190fa5ce5ba17b8d93414843ca54cde4b16dd88
iosDG001 <1870299978@qq.com>
2025-12-25 16:24:06 +0800

    更新 FliTik.js

---
+++ FliTik.js
@@ -0,0 +1,15 @@
+/*        
+        ➪：FliTik翻页时钟⏰
+
+        ★：解锁永久🆅🅸🅿
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/api\.flitik\.com\/v1\/flitik\/user\/info? url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/FliTik.js
+
+[MITM]
+hostname = api.flitik.com
+
+
+*/
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('9 1=$a.1;1=1.2(/"3"\\s*:\\s*"[^"]*"/g,\'"3" : "b-c-e:f:h"\').2(/"5"\\s*:\\s*"[^"]*"/g,\'"5" : "𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\').2(/"6"\\s*:\\s*"[^"]*"/g,\'"6" : "i://j.k.l/m/n/o/p/q.r"\').2(/"7"\\s*:\\s*\\d+/g,\'"7" : 4\').2(/"8"\\s*:\\s*\\d+/g,\'"8" : 0\');$t({1});',30,30,'|body|replace|vip_expire_time||email|head_url|vip_type|vip_scope|let|response|2099|01||01T00|00||00Z|http|oss|v2dj|com|pic|2025|07|18|17528094565169|jpeg||done'.split('|'),0,{}));
