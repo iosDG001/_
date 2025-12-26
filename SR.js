d21bb5ee763ef07cf9a012e9a89d5fde7fbcf013
iosDG001 <1870299978@qq.com>
2025-12-27 00:38:19 +0800

    更新 SR.js

---
+++ SR.js
@@ -0,0 +1,18 @@
+/*        
+        ➪：识日
+
+        ★：解锁永久🆅🅸🅿
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/shiri\.jingqueyun\.com\/MobileDevice\/getInfo url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/SR.js
+
+[MITM]
+hostname = shiri.jingqueyun.com
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('9 3=$a.3;3=3.4(/"5"\\2*:\\2*\\d+/g,\'"5" : 1\').4(/"5"\\2*:\\2*"0"/g,\'"5" : "1"\').4(/"6"\\2*:\\2*"[^"]*"/g,\'"6" : "𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\').4(/"7"\\2*:\\2*"[^"]*"/g,\'"7" : "b-8-8"\');3=3.4(/("c"\\2*:\\2*)\\d+/g,\'$e\');$f({3});',17,17,'||s|body|replace|is_vip|nick|vip_time|01|let|response|2099|duobi||1888888888|done|'.split('|'),0,{}));
+
