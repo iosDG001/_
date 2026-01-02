a9e028d0edca95a9175b337500fff2caf00f3953
iosDG001 <1870299978@qq.com>
2026-01-02 17:18:02 +0800

    更新 LGAI.js

---
+++ LGAI.js
@@ -0,0 +1,13 @@
+/*        
+        ➪：灵格AI英语
+
+        ★：解锁永久🆅🅸🅿
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/gate\.lingoq\.com\/usercenter-facade-app-prod\/users\/index? url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/LGAI.js
+[MITM]
+hostname = gate.lingoq.com
+*/
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('7 2=$8.2;2=2.4(/("9"\\3*:\\3*)6/g,\'$5\');2=2.4(/("a"\\3*:\\3*)0/g,\'$b\');2=2.4(/("c"\\3*:\\3*)6/g,\'$5\');2=2.4(/("d"\\3*:\\3*)0/g,\'$e\');2=2.4(/("f"\\3*:\\3*")[^"]*"/g,\'$1𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\');2=2.4(/("h"\\3*:\\3*)i/g,\'$5\');$j({2});',20,20,'||body|s|replace|1true|false|let|response|lifetimeVip|signInDays|1888|vip|vipLeftDays|1999|nickName||showIosEvaluationDialog|true|done'.split('|'),0,{}));
