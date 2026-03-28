1922ef0dccaec02e37655c866be2b50a35876ba8
iosDG001 <1870299978@qq.com>
2026-03-28 12:46:38 +0800

    更新 WWMH.js

--- WWMH.js
+++ WWMH.js
@@ -12,4 +12,4 @@ hostname = api.superwao.cn
 
 
 */
-;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 0=$6.0;7.8(0);0=0.3(/("9"\\2*:\\2*)\\d+/4,\'$a\');0=0.3(/("b"\\2*:\\2*")[^"]*"/4,\'$1𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\');$c({0});',14,14,'body||s|replace|g|let|response|console|log|membershipType|12|username|done|'.split('|'),0,{}));
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 0=$6.0;7.8(0);0=0.3(/("9"\\2*:\\2*)\\d+/4,\'$a\');0=0.3(/("b"\\2*:\\2*")[^"]*"/4,\'$1𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\');$c({0});',14,14,'body||s|replace|g|let|response|console|log|membershipType|13|username|done|'.split('|'),0,{}));
