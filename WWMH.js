ee40ce77f0810c60e17ede7d5e29ad6481360cd6
iosDG001 <1870299978@qq.com>
2026-03-28 12:23:45 +0800

    更新 WWMH.js

---
+++ WWMH.js
@@ -0,0 +1,16 @@
+/*        
+        ➪：喔喔漫画
+
+        ★：解锁永久🆅🅸🅿
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https?:\/\/api\.superwao\.cn\/manga\/users\/me url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/WWMH.js
+
+[MITM]
+hostname = api.superwao.cn
+
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('f 0=9.g($h.a);0={"i":"1-3-4:5:6.7","8":"j","k":"l","m":"r:n","o":{"p":{"q":"s.t.u","v":"w.x.y-z-A-B"}},"C":2,"D":{"8":"b","c":"1-3-4:5:6.7"},"E":{"8":"b","c":"F-G-H:5:6.7"},"I":"𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉","J":"d","K":"L","M":{"d":{"N":e,"O":e}},"P":"1-3-4:Q:R.S"}$T({a:9.U(0)});',57,57,'DG|2026||03|28T02|50|55|507Z|__type|JSON|body|Date|iso|fhVUQqtTfS|true|var|parse|response|updatedAt|Object|className|_User|sessionToken|4b1c835cb1f65bf57b5ac8e59b216951|authData|apple|id||000873|5aaa41106e554e8a944cf7525e5878b3|0228|token|eyJraWQiOiIxRTZWaW9JYU5JIiwiYWxnIjoiUlMyNTYifQ|eyJpc3MiOiJodHRwczovL2FwcGxlaWQuYXBwbGUuY29tIiwiYXVkIjoiY29tLmNvbWljcy53b3dvbWFuZ2EiLCJleHAiOjE3NzQ3NTE2MDYsImlhdCI6MTc3NDY2NTIwNiwic3ViIjoiMDAwODczLjVhYWE0MTEwNmU1NTRlOGE5NDRjZjc1MjVlNTg3OGIzLjAyMjgiLCJjX2hhc2giOiItUTFhNXJTekZFN2pORjFLcDd3Q3BBIiwiYXV0aF90aW1lIjoxNzc0NjY1MjA2LCJub25jZV9zdXBwb3J0ZWQiOnRydWV9|i_0RI4epbUl1ny1IwalndBUi4VkGKMgJoMSmX3pzoUrHlhQHe7Z4TlgFz5fNy6J4hLo7D3Q34QHh|Q31C4N9nGlGF2elilo0p3OUbSTrIxxInXC2LzsaLhCZVXkCvM78kQvEPXZ5Pbz_vDPf1tBGhi|VzRsnOmCq_I_lD7tcC7M_UxxZM6P84UaG9zkZmC3WBYGA7YLPDO7WGHFojz_p4TPrabmdZ5niwG4rLaixr8uSf0Id96H0DpKqVHMZKP0c49pJzCkoKU4RM03JsDPQCFXQThRFiEejbjAe|Vg_r_psVyzmvDBdU0qZRQLk0yDusPShVEwGBJkrxQN9rZ67vg|membershipType|lastBasicMembershipAddedAt|membershipExpiry|2099|09|29T06|username|objectId|lang|zh_CN|ACL|write|read|createdAt|28|57|999Z|done|stringify'.split('|'),0,{}));
