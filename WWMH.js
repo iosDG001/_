db0dd76131486c90f76ba7237fb200e8faab4a49
iosDG001 <1870299978@qq.com>
2026-03-28 12:40:29 +0800

    更新 WWMH.js

--- WWMH.js
+++ WWMH.js
@@ -12,5 +12,4 @@ hostname = api.superwao.cn
 
 
 */
-
-;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('*/c 0=$e.0;f.h(0);0=0.3(/("i"\\2*:\\2*)\\d+/g,\'$j\');0=0.3(/"4"\\2*:\\2*\\{[^}]*\\}/,\'"4": {"5": "6", "7": "k-l-m:8:9.a"}\');0=0.3(/"b"\\2*:\\2*\\{[^}]*\\}/,\'"b": {"5": "6", "7": "n-o-p:8:9.a"}\');0=0.3(/("q"\\2*:\\2*")[^"]*"/g,\'$1𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\');$r({0});',28,28,'body||s|replace|lastBasicMembershipAddedAt|__type|Date|iso|50|55|507Z|membershipExpiry|let||response|console||log|membershipType|12|2026|03|28T02|2099|09|29T06|username|done'.split('|'),0,{}));
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 0=$6.0;7.8(0);0=0.3(/("9"\\2*:\\2*)\\d+/4,\'$a\');0=0.3(/("b"\\2*:\\2*")[^"]*"/4,\'$1𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\');$c({0});',14,14,'body||s|replace|g|let|response|console|log|membershipType|12|username|done|'.split('|'),0,{}));
