8c649d703361df8f4a5334f2e68d9b4b327870e2
iosDG001 <1870299978@qq.com>
2026-04-14 18:27:08 +0800

    更新 SGYY.js

---
+++ SGYY.js
@@ -0,0 +1,18 @@
+/*        
+        ➪：傻瓜英语
+
+        ★：解锁永久🆅🅸🅿
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/sgyy\.easyenglish888\.com\/user\/(?:info|get) url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/SGYY.js
+
+[MITM]
+hostname = sgyy.easyenglish888.com
+
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6 0=$7.0;0=0.2(/("8"\\s*:\\s*")[^"]*"/g,\'$1𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\');0=0.2(/("9"\\s*:\\s*")[^"]*"/,\'$a://b.c.e/f/h/i/4/j.k"\');0=0.2(/("l"\\s*:\\s*)(m|n)/,\'$o\');0=0.2(/("p"\\s*:\\s*)\\d+/,\'$q\');0=0.2(/("r"\\s*:\\s*")[^"]*"/,\'$t-5-5"\');0=0.2(/("u"\\s*:\\s*)\\d+/,\'$3\');0=0.2(/("v"\\s*:\\s*")[^"]*"/,\'$1超凡大师Ⅰ"\');0=0.2(/("w"\\s*:\\s*)\\d+/,\'$3\');0=0.2(/("x"\\s*:\\s*)\\d+/,\'$4\');0=0.2(/("y"\\s*:\\s*)\\d+/,\'$z\');0=0.2(/("A"\\s*:\\s*)\\d+/,\'$B\');0=0.2(/("C"\\s*:\\s*)\\d+/,\'$D\');0=0.2(/("E"\\s*:\\s*)\\d+/,\'$3\');0=0.2(/("F"\\s*:\\s*)\\d+/,\'$G\');H.I(\'J 0 K\');$L({0});',48,48,'body||replace|11|18|09|let|response|name|header|1http|oss|v2dj||com|pic||2025|07|17528094565169|jpeg|vip|true|false|1true|vip_type|188|vip_end_date||12999|subscription|segment_rank|user_rank|study_level|s_level|19|profession|16|jump_type|17|vip_style|gemstone|199999999|console|log|Rewritten|successfully|done'.split('|'),0,{}));
