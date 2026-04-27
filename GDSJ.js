86048b3ce3610e33f50459fbbf41e60f45c691aa
iosDG001 <1870299978@qq.com>
2026-04-27 17:23:53 +0800

    更新 GDSJ.js

---
+++ GDSJ.js
@@ -0,0 +1,18 @@
+/*        
+        ➪：稿定设计
+
+        ★：解锁模块使用及去水印
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+^https:\/\/www\.gaoding\.com\/api\/(structure\/company\/current|ccm\/editors\/risk_materials) url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/GDSJ.js
+
+
+[MITM]
+hostname = www.gaoding.com
+
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('5 3=$o.3;5 8=\'/p/6\';5 9=\'q\';a(3.b(8)!=-1){c 0=$d.0;0=0.2(/"e":\\4+/g,\'"e":r\');0=0.2(/"6":\\[.*?\\]/g,\'"6":[]\');0=0.2(/"f":\\[.*?\\]/g,\'"f":[]\');0=0.2(/"h":\\[.*?\\]/g,\'"h":[]\');0=0.2(/"i":\\4+/g,\'"i":s\');$7({0})}j a(3.b(9)!=-1){c 0=$d.0;0=0.2(/"k":"免费版·个人"/g,\'"k":"团队版·终身"\');0=0.2(/"l":\\4+/g,\'"l":m\');0=0.2(/"n":\\4+/g,\'"n":m\');$7({0})}j{$7({})}',29,29,'body||replace|url|w|const|risk_materials|done|sy|my|if|indexOf|let|response|has_purchased|risk_fonts||product_materials|user_right|else|product_name|is_expired|true|lifetime_vip|request|editors|api_info_types|false|null'.split('|'),0,{}));
