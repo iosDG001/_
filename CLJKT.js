cfc662168e6a45f6343695de5e7ccfc282db7868
iosDG001 <1870299978@qq.com>
2026-05-07 18:48:37 +0800

    更新 CLJKT.js

---
+++ CLJKT.js
@@ -0,0 +1,18 @@
+/*        
+        ➪：车轮驾考通
+
+        ★：解锁部分🆅🅸🅿功能-<必学500题已解锁>
+
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?://(chelun\.eclicks\.cn/user/logininfo|kjz-vip\.chelun\.com/v1/theory/status|kaojiazhao\.eclicks\.cn/xc_v7/user/user_info).*$ url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/CLJKT.js
+
+[MITM]
+hostname = chelun.eclicks.cn, kjz-vip.chelun.com, kaojiazhao.eclicks.cn
+
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('h 2=$i.2;4.5(2);2=2.3(/"6"\\s*:\\s*\\d+/g,\'"6": 1\');2=2.3(/"7"\\s*:\\s*\\d+/g,\'"7": 0\');2=2.3(/"8"\\s*:\\s*"[^"]*"/g,\'"8": "𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\');2=2.3(/"9"\\s*:\\s*"[^"]*"/g,\'"9": "j://k.l.m/n/o/p/q/r.t"\');2=2.3(/"a"\\s*:\\s*\\d+/g,\'"a": 1\');2=2.3(/"b"\\s*:\\s*\\d+/g,\'"b": 1\');2=2.3(/"c"\\s*:\\s*\\d+/g,\'"c": u\');2=2.3(/"e"\\s*:\\s*\\d+/g,\'"e": v\');2=2.3(/"f"\\s*:\\s*\\d+/g,\'"f": 0\');4.5(2);$w({2});',33,33,'||body|replace|console|log|is_vip|is_expire|nick|avatar|is_vip3|vipkc_type|open_time||expire_time|is_expired||let|response|http|oss|v2dj|com|pic|2025|07|18|17528094565169||jpeg|1778131112|4102444800|done'.split('|'),0,{}));
