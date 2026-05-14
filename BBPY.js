360e27f1809303fd46722f8be3cb9a058e950663
iosDG001 <1870299978@qq.com>
2026-05-14 22:37:19 +0800

    更新 BBPY.js

---
+++ BBPY.js
@@ -0,0 +1,17 @@
+/*        
+        ➪：帮帮拼音
+
+        ★：解锁🆅🅸🅿
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+https://api\.kidsbangbang\.cn/pinyin-api/[\w/]+\? url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/BBPY.js
+
+[MITM]
+hostname = api.kidsbangbang.cn
+
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('n 0=$o.0;7.8(0);0=0.3(/"9"\\2*:\\2*4/g,\'"9": 5\');0=0.3(/"a"\\2*:\\2*4/g,\'"a": 5\');0=0.3(/"b"\\2*:\\2*4/g,\'"b": 5\');0=0.3(/"c"\\2*:\\2*4/g,\'"c": 5\');0=0.3(/"e"\\2*:\\2*\\d+/g,\'"e": p\');0=0.3(/"f"\\2*:\\2*\\d+/g,\'"f": h\');0=0.3(/"i"\\2*:\\2*\\d+/g,\'"i": h\');0=0.3(/"j"\\2*:\\2*\\d+/g,\'"j": 1\');0=0.3(/"k"\\2*:\\2*\\d+/g,\'"k": 1\');0=0.3(/"l"\\2*:\\2*\\d+/g,\'"l": 6\');0=0.3(/"m"\\2*:\\2*"[^"]*"/g,\'"m": "q"\');7.8(0);$r({0});',28,28,'body||s|replace|false|true||console|log|isVip|isLongestVip|isPurchased|isBuyFeedSku||expiryTimestamp|expiredDays||99999|vipBalance|isVirtual|status|grade|nickname|let|response|4102444800|iOSAWJ|done'.split('|'),0,{}));
