5cdcc32b09b4ae427d958fb79a6359498fa4e923
iosDG001 <1870299978@qq.com>
2026-05-15 21:15:59 +0800

    更新 XZWXDT.js

---
+++ XZWXDT.js
@@ -0,0 +1,17 @@
+/*        
+        ➪：新知卫星地图
+
+        ★：解锁永久🆅🅸🅿
+𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹𒊹
+
+[rewrite_local] 
+
+^https?:\/\/a\.xinzhi\.space\/api\/User\/getUserByToken url script-response-body https://raw.githubusercontent.com/iosDG001/_/refs/heads/main/XZWXDT.js
+
+[MITM]
+hostname = a.xinzhi.space
+
+
+*/
+
+;eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('u 0=$v.0;8.9(0);0=0.2(/"a"\\s*:\\s*\\d+/g,\'"a": 1\');0=0.2(/"b"\\s*:\\s*\\d+/g,\'"b": 1\');0=0.2(/"c"\\s*:\\s*\\d+/g,\'"c": 1\');0=0.2(/"e"\\s*:\\s*4/g,\'"e": "f-5-5 h:3:3"\');0=0.2(/"i"\\s*:\\s*4/g,\'"i": "f-5-5 h:3:3"\');0=0.2(/"j"\\s*:\\s*\\d+/g,\'"j": k\');0=0.2(/"l"\\s*:\\s*\\d+/g,\'"l": k\');0=0.2(/"6"\\s*:\\s*"[^"]*"/g,\'"6": "m"\');0=0.2(/"7"\\s*:\\s*"[^"]*"/g,\'"7": "m"\');0=0.2(/"n"\\s*:\\s*4/g,\'"n": "o-p-w p:3:x"\');0=0.2(/"q"\\s*:\\s*y/g,\'"q": z\');0=0.2(/"r"\\s*:\\s*"[^"]*"/g,\'"r": "𝐓𝐆频道@𝐢𝐎𝐒𝐀𝐖𝐉"\');0=0.2(/"t"\\s*:\\s*4/g,\'"t": "A://B.C.D/E/o/F/G/H.I"\');0=0.2(/"6"\\s*:\\s*"[^"]*"/g,\'"6": "终身会员"\');0=0.2(/"7"\\s*:\\s*"[^"]*"/g,\'"7": "终身会员"\');8.9(0);$J({0});',46,46,'body||replace|59|null|09|vip_type|svip_type|console|log|is_vip|is_svip|is_forever_vip||vip_expire|2099||23|svip_expire|vip_day|99999|svip_day|forever|pay_time|2025|11|wx_pay|nickname||avatar|let|response|16|42|false|true|http|oss|v2dj|com|pic|07|18|17528094565169|jpeg|done'.split('|'),0,{}));
